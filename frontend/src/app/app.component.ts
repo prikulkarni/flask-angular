abb0faa3e89361c3f94444012ada9f434dfc1b53
Vibrosonic <priyanka.k.1589@gmail.com>
2018-07-15 22:16:08 +0200

    integrating Flask and Angular - some errors corrected

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import {Subscription} from 'rxjs/Subscription';
+import {Subscription} from 'rxjs';
 import {ExamsApiService} from './exams/exams-api.service';
 import {Exam} from './exams/exam.model';
 
